156ebfe82757b7802c80f76a397cb1ec06139d7e
Llewellyn Collins <llewellyn.collins@live5gaming.com>
2018-08-22 09:06:12 +0100

    - ES6ify server.js - Fix npm vulnerabilities - Add .gitignore

--- server.js
+++ server.js
@@ -1,41 +1,49 @@
-var express = require('express'),
-    request = require('request'),
-    bodyParser = require('body-parser'),
-    app = express();
+const express = require( 'express' );
+const request = require( 'request' );
+const bodyParser = require( 'body-parser' );
+const app = express();
 
-var myLimit = typeof(process.argv[2]) != 'undefined' ? process.argv[2] : '100kb';
-console.log('Using limit: ', myLimit);
+const myLimit = typeof(process.argv[ 2 ]) != 'undefined' ? process.argv[ 2 ] : '100kb';
+console.log( 'Using limit: ', myLimit );
 
-app.use(bodyParser.json({limit: myLimit}));
+app.use( bodyParser.json( { limit: myLimit } ) );
 
-app.all('*', function (req, res, next) {
+app.all( '*', ( req, res, next ) => {
 
-    // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
-    res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
+    // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production
+    // environment
+    res.header( "Access-Control-Allow-Origin", "*" );
+    res.header( "Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE" );
+    res.header( "Access-Control-Allow-Headers", req.header( 'access-control-request-headers' ) );
 
-    if (req.method === 'OPTIONS') {
+    if ( req.method === 'OPTIONS' ) {
         // CORS Preflight
         res.send();
     } else {
-        var targetURL = req.header('Target-URL');
-        if (!targetURL) {
-            res.send(500, { error: 'There is no Target-Endpoint header in the request' });
+        const targetURL = req.header( 'Target-URL' );
+        if ( !targetURL ) {
+            res.send( 500, { error: 'There is no Target-Endpoint header in the request' } );
             return;
         }
-        request({ url: targetURL + req.url, method: req.method, json: req.body, headers: {'Authorization': req.header('Authorization')} },
-            function (error, response, body) {
-                if (error) {
-                    console.error('error: ' + response.statusCode)
+        const url = targetURL + req.url;
+        console.log( 'Target URL', targetURL );
+        console.log( 'URL', url );
+        request( {
+                url,
+                method: req.method,
+                body: req.body,
+                headers: { 'Authorization': req.header( 'Authorization' ) }
+            },
+            ( error, response, body ) => {
+                if ( error ) {
+                    console.error( 'error: ' + response.statusCode )
                 }
-//                console.log(body);
-            }).pipe(res);
+            } ).pipe( res );
     }
-});
+} );
 
-app.set('port', process.env.PORT || 3000);
+app.set( 'port', process.env.PORT || 15000 );
 
-app.listen(app.get('port'), function () {
-    console.log('Proxy server listening on port ' + app.get('port'));
-});
+app.listen( app.get( 'port' ), function () {
+    console.log( 'Proxy server listening on port ' + app.get( 'port' ) );
+} );
